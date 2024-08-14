c97e09ec9419ccacfa83c3c0a576ebe0f6d1459f
Matt Zhang <set.stun@gmail.com>
2024-08-13 23:10:40 -0500

    Dumping writing samples

--- server/writing.py
+++ server/writing.py
@@ -11,27 +11,41 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/index", response_class=HTMLResponse)
 def index(request: Request):
-    story_names = [
-        path.name for path in Path("writing").iterdir() if path.is_dir()
+    pieces = [
+        (path.name, True) if path.is_dir()
+        else (path.stem, False)
+        for path in Path("writing").iterdir()
     ]
     return templates.TemplateResponse(
         request=request,
         name="pages/writing_index.html",
         context={
-            "story_names": story_names,
+            "pieces": pieces,
         }
     )
 
 
-@router.get("/{story_name}/{page}", response_class=HTMLResponse)
-def story_page(story_name: str, page: int, request: Request):
-    next_page_exists = Path(f"writing/{story_name}/{page + 1}.txt").exists()
-    current_page_content = Path(f"writing/{story_name}/{page}.txt").read_text()
+@router.get("/{piece}", response_class=HTMLResponse)
+def piece(piece: str, request: Request):
     return templates.TemplateResponse(
         request=request,
         name="pages/story.html",
         context={
-            "story_name": story_name,
+            "piece": piece,
+            "content": Path(f"writing/{piece}.txt").read_text()
+        },
+    )
+
+
+@router.get("/{piece}/{page}", response_class=HTMLResponse)
+def story_page(piece: str, page: int, request: Request):
+    next_page_exists = Path(f"writing/{piece}/{page + 1}.txt").exists()
+    current_page_content = Path(f"writing/{piece}/{page}.txt").read_text()
+    return templates.TemplateResponse(
+        request=request,
+        name="pages/story_page.html",
+        context={
+            "piece": piece,
             "current_page": page,
             "previous_page": page - 1 if page != 0 else None,
             "next_page": page + 1 if next_page_exists else None,
